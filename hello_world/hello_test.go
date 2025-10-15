9e01aab279b0bd39e8a31b5fc85ce2f74ad93214
Kode <olorik362@gmail.com>
2025-10-15 06:24:48 +0100

    feat(hello_world): add greeting support for French and Spanish
    - The greet function now supports greetings in English, French, and Spanish.

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -1,11 +1,44 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got string, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got: %q want %q", got, want)
+
+		}
+
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Kendrick", "")
+		want := "Hello, Kendrick"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Gane", "French")
+		want := "Bonjour, Gane"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
